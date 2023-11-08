42bb6bd470d17f0c59a97d78a80635d1b68b11e0
iannico322 <iannicocaulin@gmail.com>
2023-11-08 16:57:38 +0800

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,12 @@ function App() {
 
   const [quotes, SetQuotes] = useState([])
 
+  useEffect(()=>{
+
+    console.log(quotes)
+
+  },[quotes])
+
   function GetQuotes() {
     const options = {
       method: 'POST',
@@ -17,14 +23,14 @@ function App() {
         'X-RapidAPI-Key': '096d7043d4msh2675ca63ad13d15p1e2935jsnacda8f927c83',
         'X-RapidAPI-Host': 'quotel-quotes.p.rapidapi.com'
       },
-      data: {ids: [879890, 54]}
+      data: {ids: [879890, Math.floor(Math.random() * (1000 - 0 + 1)) + 0]}
     };
     
     try {
       axios.request(options).then(
         (response)=>{
         console.log(response.data);
-        SetQuotes(response.data)
+        SetQuotes([...quotes,...response.data])
       }
       );
      
@@ -37,17 +43,20 @@ function App() {
 
 
   return (
-    <div className=" h-screen w-screen  flex flex-col justify-between items-start gap-5 bg-[#E5E5E5]">
+    <div className=" h-screen w-screen  flex flex-col overflow-x-hidden justify-between items-start gap-5 bg-[#E5E5E5]">
 
       <button onClick={GetQuotes}>Get Quote</button>
 
-      <div className=" flex flex-wrap  w-full bg-[#E5E5E5] h-[80%] gap-10 justify-center">
+      <div className=" flex flex-wrap  w-full bg-[#E5E5E5] min-h-[100px] gap-10 justify-center">
 
 
         {quotes.map((e)=>(
 
+          
+
           <Card
           text={e.quote}
+          name={e.name}
           />
         ))}
     
--- src/components/icons/card/card.jsx
+++ src/components/icons/card/card.jsx
@@ -5,7 +5,7 @@ const Card = (props) => {
     <div className=" relative w-[428px] h-[209px] bg-[#363062] rounded-lg p-5 border-2 text-center flex items-center justify-center  ">
        <p>{props.text}</p>  
 
-        <p className=' absolute bottom-0 right-0 p-10 '>- Nico</p>
+        <p className=' absolute bottom-0 right-0 p-10 '>- {props.name}</p>
     </div>
   )
 }
